26424091b8b78074229d55f8a494982ac28383e4
Shubham sayaji jadhav <142787884+shubhamjadhav07@users.noreply.github.com>
2024-09-17 23:53:17 +0530

    Time: 20 ms (75.85%) | Memory: 51 MB (34.16%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,17 +3,13 @@ class Solution {
         int m = text1.length();
         int n = text2.length();
 
-        // Create a DP table with dimensions (m+1) x (n+1)
         int[][] dp = new int[m + 1][n + 1];
 
-        // Fill the DP table using bottom-up approach
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                // If characters match, add 1 to the previous diagonal value
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    // Otherwise, take the maximum from the top or left
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
