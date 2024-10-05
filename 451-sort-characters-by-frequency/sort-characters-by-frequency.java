7c312296856a1c1f9b3abbdaa20f9b57dd0d5dae
Shubham sayaji jadhav <142787884+shubhamjadhav07@users.noreply.github.com>
2024-10-05 19:39:24 +0530

    Time: 5 ms (95.46%) | Memory: 45 MB (86.92%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,27 +1,18 @@
 class Solution {
     public String frequencySort(String s) {
-        int[][] freqArray = new int[128][2]; // [0]: char (ASCII value), [1]: frequency
-
-        // Step 2: Count the frequency of each character in the string
-        for (char c : s.toCharArray()) {
-            freqArray[c][0] = c;  // Store the character's ASCII value
-            freqArray[c][1]++;    // Increment the frequency
+        int[][] nums= new int[128][2];
+        for(char ch:s.toCharArray()){
+            nums[ch][0]= ch;
+            nums[ch][1]++;
         }
-
-        // Step 3: Sort the frequency array by the frequency values in decreasing order
-        Arrays.sort(freqArray, (a, b) -> b[1] - a[1]); // Sort by frequency in decreasing order
-
-        // Step 4: Build the result string based on sorted frequencies
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < freqArray.length; i++) {
-            int frequency = freqArray[i][1];
-            if (frequency > 0) {  // Only consider characters that appear in the string
-                for (int j = 0; j < frequency; j++) {
-                    result.append((char) freqArray[i][0]);
-                }
+        Arrays.sort(nums,(a,b)->b[1]-a[1]);
+        StringBuilder str= new StringBuilder();
+        for(int i=0;i<128;i++){
+            int freq= nums[i][1];
+            for(int j=freq-1;j>=0;j--){
+                str.append((char)nums[i][0]);
             }
         }
-
-        return result.toString();
+        return str.toString();
     }
 }
