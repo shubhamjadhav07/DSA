a3632e2ba990f144fef735c4e73a7fa0f037ef50
Shubham sayaji jadhav <142787884+shubhamjadhav07@users.noreply.github.com>
2026-01-14 22:22:24 +0530

    Time: 634 ms (45.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+SELECT s.user_id as user_id, ROUND(AVG(IF(c.action='confirmed',1,0)),2) as confirmation_rate 
+FROM Signups s
+LEFT JOIN Confirmations  c
+ON s.user_id=c.user_id
+GROUP BY 1
+ORDER BY 2
+
+
+
+
+
+
